cebe7dad65eb6006503022b590905aa3122ee0b4
Iamdami48 <96474713+Iamdami48@users.noreply.github.com>
2021-12-30 08:16:30 -0800

    Dami-Meso-Gram

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>Dami & Meso Gram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Lorem ipsum  sit amet, consectetur adipiscing elit.</p>
     </div>
   )
 }
